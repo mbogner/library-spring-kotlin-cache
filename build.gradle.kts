dce75a692153edbe0a411f730f1eb259ab850812
Manuel Bogner <m@mbo.dev>
2025-07-05 13:11:58 +0200

    upgrade libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("org.jetbrains.dokka")
     jacoco
     id("org.sonarqube")
-    signing // required for maven central
     id("maven-publish")
     id("net.researchgate.release")
     id("org.jreleaser")
@@ -80,10 +79,13 @@ tasks {
         archiveClassifier.set("javadoc")
         from(named<org.jetbrains.dokka.gradle.DokkaTask>("dokkaJavadoc").get().outputDirectory)
     }
-
+    
+    named("jreleaserFullRelease") {
+        dependsOn("publish")
+    }
+    
     named("afterReleaseBuild") {
         dependsOn(
-            "signMavenPublication",
             "publishToMavenLocal",
             "jreleaserFullRelease"
         )
@@ -113,8 +115,19 @@ jreleaser {
     deploy {
         maven {
             mavenCentral {
-                active.set(org.jreleaser.model.Active.ALWAYS)
+                register("sonatype") {
+                    active.set(org.jreleaser.model.Active.ALWAYS)
+                    url.set("https://central.sonatype.com/api/v1/publisher")
+                    snapshotSupported.set(true)
+                    stagingRepository("${layout.buildDirectory.get()}/staging-deploy")
+                }
             }
+        }
+    }
+    release {
+        github {
+            tagName.set("{{projectVersion}}")
+            releaseName.set("{{projectVersion}}")
         }
     }
 }
@@ -160,8 +173,10 @@ publishing {
             }
         }
     }
-}
-
-signing {
-    sign(publishing.publications["maven"])
+    repositories {
+        maven {
+            name = "staging"
+            url = uri("${layout.buildDirectory.get()}/staging-deploy")
+        }
+    }
 }
